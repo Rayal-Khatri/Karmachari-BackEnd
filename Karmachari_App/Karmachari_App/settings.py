db3f757a61164c4aa737d1520b6856f3435f4ccb
Sumit Deshar <sumeetdsr@gmail.com>
2023-01-23 10:55:31 +0545

    adding django debugging toolbar

--- Karmachari_App/Karmachari_App/settings.py
+++ Karmachari_App/Karmachari_App/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
 ]
 
 ROOT_URLCONF = 'Karmachari_App.urls'
