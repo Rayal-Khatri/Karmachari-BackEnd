1c4e7f1c946f632da1ff7f83a426e3015ab96a58
sumit deshar <sumeetdsr@gmail.com>
2023-02-11 22:36:32 +0545

    updated template and bug fix on navbar

--- Karmachari_App/mainapp/admin.py
+++ Karmachari_App/mainapp/admin.py
@@ -4,3 +4,9 @@ from .models import *
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Notice)
+admin.site.register(Department)
+admin.site.register(Leaves)
+admin.site.register(Calendar)
+admin.site.register(Salary)
+admin.site.register(Schedule)
+admin.site.register(Payroll)
